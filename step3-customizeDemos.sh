0f012b08675dd24bee4a746fc837e9471dee72be
dansand71 <dansand71@hotmail.com>
2017-03-07 18:40:10 -0500

    99

--- step3-customizeDemos.sh
+++ step3-customizeDemos.sh
@@ -3,10 +3,10 @@ echo "Please customize the line below to build your customized demo environment.
 echo "This should be run on the LINUX Utility server after the initial environment setup."
 
 echo "Change the server names"
-sudo sed -i 's/REPLACEME/[lowercasename]/g' *.*
+sudo grep -rl REPLACEME ./ | sudo xargs sed -i 's/REPLACEME/dansand/g'
 
-echo "Change the Azure Insights Passworkd"
-sudo sed -i 's/[REPLACE-REGISTRY-PASSWORD]/[NEW REGISTRY PASSWORD FROM PORTAL]/g' *.*
+echo "Change the REGISTRYPASSWORD Password"
+sudo grep -rl REPLACEME ./ | sudo xargs sed -i 's/REPLACEME/dansand/g'
 
-echo "Change the server names"
-sudo sed -i 's/[REPLACE-APP-INSIGHTS-KEY]/[NEW APP INSIGHTS KEY FROM PORTAL]/g' *.*
+echo "Change the APPINSIGHTKEY names"
+sudo grep -rl REPLACEME ./ | sudo xargs sed -i 's/REPLACEME/dansand/g'
