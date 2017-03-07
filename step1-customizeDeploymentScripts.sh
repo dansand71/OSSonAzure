95afe9faca6ec514eaedae0dfa316bf138667f76
dansand71 <dansand71@hotmail.com>
2017-03-07 16:07:40 -0500

    85

--- step1-customizeDeploymentScripts.sh
+++ step1-customizeDeploymentScripts.sh
@@ -3,9 +3,3 @@ echo "Please customize the line below to build your customized scripts..."
 
 echo "Change the server names"
 sudo sed -i 's/[VAR1]/dansand/g' *.*
-
-echo "Change the server names"
-sudo sed -i 's/[VAR1]/dansand/g' *.*
-
-echo "Change the server names"
-sudo sed -i 's/[VAR1]/dansand/g' *.*
