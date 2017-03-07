3c1a59df87a57d10cfd276e37e7ee80cdbcf987f
dansand71 <dansand71@hotmail.com>
2017-03-06 23:44:27 -0500

    56

--- createAzureEnvironment.sh
+++ createAzureEnvironment.sh
@@ -41,3 +41,5 @@ echo ""
 echo 'Create CENTOS utility machine'
 az vm create -g 'utility' -n centos-utility --public-ip-address-dns-name 'gbboss-centos-utility' --os-disk-name 'centos-utility-disk' --image "OpenLogic:CentOS:7.2:latest" --os-type linux --nsg 'NSG-utility'  --storage-sku 'Premium_LRS' --size Standard_DS2_v2  --admin-username GBBOSSDemo --ssh-key-value 'ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAz7ItfqCoqLGGbSdNT52SrZvIO2Fc26yUUyPxohN4IYxUcc1O9tmXzxHwah0jwMOw6ux+JbycOEiEpxoYPLOe9R98cKMyilnL9hGs6jCmVmRLuc/ny76euR2t8v0lhGT1yTrkLpwIlfkcaDqpufkIqQmqd20NlWbdHzsYA+s++e3jIgE5qJwO/InlMvv90nkPftR/PRYq7etWgImi00qQgX1VcD8NMZzm1qC4unzEQhYbIqYAgScCzeaj5U5NSOvDm6wgwceBCcdM8jSm7SYdetVm3J3cd+hO+SVKYgx8Zg1+kdh9RkaE2+ZRr0wtoUi/ClOXb53a4rtfYYzj85/W9w== rsa-key-20170222'
 
+#az vm boot-diagnostics enable -n centos-utility -g utility
+
