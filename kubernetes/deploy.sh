10718fe75029b834535eb2e020df60de188f856b
dansand71 <dansand71@hotmail.com>
2017-03-08 16:19:37 -0500

    111

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Initial deployment & expose the service"
 #az account set --subscription "Microsoft Azure Internal Consumption"
-kbuectl create -f /source/OSSonAzure/kubernetes/aspnet-core-linux-deploy.yml
+kubectl create -f /source/OSSonAzure/kubernetes/aspnet-core-linux-deploy.yml
 
 echo "Initial deployment & expose the service"
 kubectl expose deployments aspnet-core-linux-deployment --port=80 --target-port=5000 --type=LoadBalancer --name=aspnet-core-linux
