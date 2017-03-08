1da0b8b6c6e593109b98ed1653a7f73c87de18c9
dansand71 <dansand71@hotmail.com>
2017-03-08 18:15:44 -0500

    asdf

--- step1-setupTemplate.sh
+++ step1-setupTemplate.sh
@@ -2,7 +2,7 @@
 #This script is meant to be run on your initial environment LINUX CENTOS or UBUNTU setup machine
 
 echo "Please customize the line below to build your customized scripts..."
-mkdir /source
+sudo mkdir ./source
 echo "Downloading the demos from GIT and change ownership"
 
 #Install and configure GIT
@@ -14,18 +14,17 @@ if [ -f /etc/lsb-release ]; then
   sudo apt-get install git
 fi
 
-sudo mkdir source
 cd ./source
 sudo rm -rf ./source/OSSonAzure
 sudo git clone https://github.com/dansand71/OSSonAzure
+sudo chown -R GBBOSSDemo ./source/OSSonAzure/.
 
 #CHANGE your VALUES HERE
 echo "Change the server names"
-sudo grep -rl REPLACEME ./ | sudo xargs sed -i 's/REPLACEME/new-short-lowercase-new-value/g'
+sudo grep -rl REPLACEME ./source --exclude step1-setupAzure.sh| sudo xargs sed -i 's/REPLACEME/new-short-lowercase-new-value/g'
 
 echo "Change the REGISTRY NAME"
-sudo grep -rl REPLACEME ./ | sudo xargs sed -i 's/REPLACE-REGISTRY-NAME/new-registry-name-from-portal/g'
+sudo grep -rl REPLACEME ./source --exclude step1-setupAzure.sh| sudo xargs sed -i 's/REPLACE-REGISTRY-NAME/new-registry-name-from-portal/g'
 
 #Set Scripts as executable
-sudo chmod +x /source/OSSonAzure/step1-customizeDeployemntScripts.sh
-sudo chmod +x /source/OSSonAzure/step2-createAzureDemoEnvironment.sh
+sudo chmod +x ./source/OSSonAzure/step2-createAzureDemoEnvironment.sh
