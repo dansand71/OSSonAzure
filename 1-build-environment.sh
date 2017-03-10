8b8da88ef1f15fac38a349a3bbca0019202fe1cf
dansand71 <dansand71@hotmail.com>
2017-03-10 17:45:37 -0500

    88

--- 1-build-environment.sh
+++ 1-build-environment.sh
@@ -1,59 +1,101 @@
 #!/bin/bash
-#This script is meant to be run on your initial environment LINUX CENTOS or UBUNTU setup machine
+echo "Welcome to the OSS Demo Jumpbox install process.  This script will:"
+echo "    - Install git"
+echo "    - Install Azure CLI if not present"
+echo "    - Log in to Azure and create a Resource Group 'ossdemo-utility' and CENTOS VM"
+echo ""
+echo "Installation will require SU rights."
+echo ""
+echo "Installing git & ansible if they are missing."
 
-#Install AZ Client - can we check if it is already installed?
-curl -L https://aka.ms/InstallAzureCli | bash 
-
-sudo mkdir /source
-
-#Install GIT - can we check if it is already installed?
-#Install and configure GIT
+ssh GBBOSSDemo@jumpbox-dansand71.eastus.cloudapp.azure.com 'echo -e "AzureForOSS\nAzureForOSS" | passwd GBBOSSDemo'
+#Check DISTRO
 if [ -f /etc/redhat-release ]; then
+  sudo yum update -y
   sudo yum -y install git
 fi
-
 if [ -f /etc/lsb-release ]; then
-  sudo apt-get install git
+  gitinfo=$(dpkg-query -W -f='${Package} ${Status} \n' git | grep "git install ok installed")
+  if [[ $gitinfo =~ "git install ok installed" ]]; then
+     echo "   git installed - skipping"
+  else  
+     echo "   could not find git - installing...."
+     sudo apt-get install git -y
+  fi
+  sudo apt-get install software-properties-common -y
+  sudo apt-add-repository ppa:ansible/ansible -y
+  sudo apt-get update -y
+  sudo apt-get install ansible -y
+  sudo apt-get update && apt-get install -y libssl-dev libffi-dev python-dev
 fi
 
-#Remove existing infra & reset
-cd /source
-sudo rm -rf /source/OSSonAzure
-sudo git clone https://github.com/dansand71/OSSonAzure
-sudo chown -R GBBOSSDemo /source/OSSonAzure/.
-
-
-#Check if we are logged in - if 'az group list' returns "Please run 'az login' to setup account."
-# run az login
-echo "Azure Login"
-az login
-
-#If they type anything then try and change subscrition to that value.  If blank skip...
-echo "Change Azure Subscription to:"
-read azSubscriptionName
-if [ -z azSubscriptionName ]; then
-  az account set --subscription "Microsoft Azure Internal Consumption"
+echo ""
+echo "Installing AZ command line tools if they are missing."
+#Check to see if Azure is installed if not do it...
+if [ -f ~/bin/az ]
+  then
+    echo "    AZ Client installed. Skipping install.."
+  else
+    curl -L https://aka.ms/InstallAzureCli | bash
+    exec -l $SHELL
+fi
+clear
+echo ""
+echo "Logging in to Azure"
+#Checking to see if we are logged into Azure
+echo "    Checking if we are logged in to Azure."
+#We need to redirect the output streams to stdout
+azstatus=`az group list 2>&1` 
+if [[ $azstatus =~ "Please run 'az login' to setup account." ]]; then
+   echo "   We need to login to azure.."
+   az login
+else
+   echo "    Logged in."
+fi
+read -p "    Change default subscription? [y/n]:" changesubscription
+if [[ $changesubscription =~ "y" ]];then
+    read -p "      New Subscription Name:" newsubscription
+    az account set --subscription "$newsubscription"
+else
+    echo "    Using default existing subscription."
 fi
 
-
+echo ""
+echo "Set values for creation of resource groups and jumpbox server"
 # Check the validity of the name (no dashes, spaces, less than 8 char, no special chars etc..)
 # Can we set a Enviro variable so if you want to rerun it is here and set by default?
-echo "Please enter your unique server prefix: (Jumpbox server will become:'utility-PREFIX-jumpbox')"
-echo "  Note - values should be lowercase and less than 8 characters.')"
-read serverPrefix
+echo "    Please enter your unique server prefix: (Jumpbox server will become:'utility-PREFIX-jumpbox')"
+echo "      Note - values should be lowercase and less than 8 characters.')" 
+read -p "Server Prefix:" serverPrefix
+serverPrefix=${serverPrefix,,} 
+echo ""
+
+echo ""
+echo "Set initial password for jumpbox server:"
+stty -echo
+read jumpboxPassword
+stty echo
+echo ""
 
 # Check the validity of the name (no dashes, spaces, less than 8 char, no special chars etc..)"
 # Can we set a Enviro variable so if you want to rerun it is here and set by default?
-echo "Please enter your unique storage prefix: (Storage Account will become: 'PREFIX-storage'')"
-echo "  Note - values should be lowercase and less than 8 characters.')"
-read storagePrefix
+echo "    Please enter your unique storage prefix: (Storage Account will become: 'PREFIX-storage'')"
+echo "      Note - values should be lowercase and less than 8 characters.')"
+read -e -i "$serverPrefix" -p "Storage Prefix: " storagePrefix
+storagePrefix=${storagePrefix,,}
+echo ""
 
+echo "Building environment with Server:" $serverPrefix " and Storage Account: " $storagePrefix
+read -p "Continue? [y/n]" continuescript
+if [[ $continuescript != "y" ]];then
+  echo "exiting..."
+  exit
+fi
 
 #BUILD RESOURCE GROUPS
 echo ""
 echo "BUILDING RESOURCE GROUPS"
 echo "--------------------------------------------"
-az group create --name ossdemo-kubernetes --location eastus
 echo 'create utility resource group'
 az group create --name ossdemo-utility --location eastus
 
@@ -63,7 +105,8 @@ echo "BUILDING STORAGE ACCOUNTS"
 echo "--------------------------------------------"
 echo "Create Utility Storage account - you may need to change this in case there is a conflict"
 echo "this is used in VM Create (Diagnostics storage) and Azure Registry"
-az storage account create -l eastus -n $storagePrefix-storage -g ossdemo-utility --sku Standard_LRS
+
+az storage account create -l eastus -n ${storagePrefix}storage -g ossdemo-utility --sku Standard_LRS
 
 #BUILD NETWORKS SECURTIY GROUPS and RULES
 echo ""
@@ -71,19 +114,73 @@ echo "BUILDING NETWORKS SECURTIY GROUPS and RULES"
 echo "--------------------------------------------"
 echo 'Network Security Group for utility Resource Group'
 az network nsg create --resource-group ossdemo-utility --name NSG-ossdemo-utility --location eastus
+
 echo 'Allow RDP inbound to Utility'
-az network nsg rule create --resource-group ossdemo-utility --nsg-name NSG-ossdemo-utility --name rdp-rule --access Allow --protocol Tcp --direction Inbound --priority 100 --source-address-prefix Internet --source-port-range "*" --destination-address-prefix "*" --destination-port-range 3389
-echo 'Allow SSH inbound to Utility'
-az network nsg rule create --resource-group ossdemo-utility --nsg-name NSG-ossdemo-utility --name ssh-rule --access Allow --protocol Tcp --direction Inbound --priority 110 --source-address-prefix Internet --source-port-range "*" --destination-address-prefix "*" --destination-port-range 22
+az network nsg rule create --resource-group ossdemo-utility \
+     --nsg-name NSG-ossdemo-utility --name rdp-rule \
+     --access Allow --protocol Tcp --direction Inbound --priority 100 \
+     --source-address-prefix Internet \
+     --source-port-range "*" --destination-address-prefix "*" \
+     --destination-port-range 3389
+ echo 'Allow SSH inbound to Utility'
+ az network nsg rule create --resource-group ossdemo-utility \
+     --nsg-name NSG-ossdemo-utility --name ssh-rule \
+     --access Allow --protocol Tcp --direction Inbound --priority 110 \
+     --source-address-prefix Internet \
+     --source-port-range "*" --destination-address-prefix "*" \
+     --destination-port-range 22
 
 #CREATE UTILITY JUMPBOX SERVER
 echo ""
 echo 'Creating CENTOS JUMPBOX utility machine for RDP and ssh'
 echo "--------------------------------------------"
-az vm create -g 'ossdemo-utility' -n utility-$serverPrefix-jumpbox \
-        --public-ip-address-dns-name 'utility-$serverPrefix-jumpbox' \
-        --os-disk-name 'utility-$serverPrefix-jumpbox-disk' \
-        --image "OpenLogic:CentOS:7.2:latest" --os-type linux --nsg 'NSG-ossdemo-utility'  --storage-sku 'Premium_LRS' \
+az vm create -g ossdemo-utility -n jumpbox-${serverPrefix} \
+        --public-ip-address-dns-name jumpbox-${serverPrefix} \
+        --os-disk-name jumpbox-${serverPrefix}-disk \
+        --image "OpenLogic:CentOS:7.2:latest" --os-type linux --nsg NSG-ossdemo-utility  --storage-sku Premium_LRS \
         --size Standard_DS1_v2 --admin-username GBBOSSDemo \
-        --no-wait \
         --ssh-key-value 'ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAz7ItfqCoqLGGbSdNT52SrZvIO2Fc26yUUyPxohN4IYxUcc1O9tmXzxHwah0jwMOw6ux+JbycOEiEpxoYPLOe9R98cKMyilnL9hGs6jCmVmRLuc/ny76euR2t8v0lhGT1yTrkLpwIlfkcaDqpufkIqQmqd20NlWbdHzsYA+s++e3jIgE5qJwO/InlMvv90nkPftR/PRYq7etWgImi00qQgX1VcD8NMZzm1qC4unzEQhYbIqYAgScCzeaj5U5NSOvDm6wgwceBCcdM8jSm7SYdetVm3J3cd+hO+SVKYgx8Zg1+kdh9RkaE2+ZRr0wtoUi/ClOXb53a4rtfYYzj85/W9w== rsa-key-20170222'
+
+#Download the GIT Repo for keys etc.
+echo "--------------------------------------------"
+echo "Downloading the Github repo for the connectivity keys and bits."
+sudo mkdir /source
+cd /source
+sudo rm -rf /source/OSSonAzure
+sudo git clone https://github.com/dansand71/OSSonAzure
+
+#Created Server - now SSH into the server
+echo "We are copying in a new private key.  If we find an existing id_rsa we will make a copy and cleanup after."
+if [ -f ~/.ssh/id_rsa ]
+  then
+    echo "    Existing private key found.  Making a copy for backup id_rsa_OSSDEMOBACKUP"
+    sudo mv ~/.ssh/id_rsa ~/.ssh/id_rsa_OSSDEMOBACKUP
+fi
+sudo cp /source/OSSonAzure/ssh-keys/id_rsa ~/.ssh/id_rsa
+sudo chmod 600 ~/.ssh/id_rsa
+sudo chown $USER ~/.ssh/id_rsa
+
+echo "--------------------------------------------"
+echo "Configure jumpbox server with ansible"
+sudo sed -i -e "s/JUMPBOXSERVER-REPLACE/jumpbox-${serverPrefix}.eastus.cloudapp.azure.com/g" /source/OSSonAzure/ansible/hosts
+ansible-playbook -i /source/OSSonAzure/ansible/hosts /source/OSSonAzure/ansible/jumpbox-server-configuration.yml
+
+#Set the remote jumpbox passwords
+ssh GBBOSSDemo@jumpbox-${serverPrefix}.eastus.cloudapp.azure.com 'echo "GBBOSSDemo:${jumpboxPassword}" | sudo chpasswd'
+ssh GBBOSSDemo@jumpbox-${serverPrefix}.eastus.cloudapp.azure.com 'echo "root:${jumpboxPassword}" | sudo chpasswd'
+
+echo ""
+echo "Cleaning up the RSA Keys"
+#CLEANUP & Finish
+if [ -f ~/.ssh/id_rsa_OSSDEMOBACKUP ]
+  then
+    echo "    Restoring previous SSH private key."
+    sudo rm ~/.ssh/id_rsa
+    sudo mv ~/.ssh/id_rsa_OSSDEMOBACKUP ~/.ssh/id_rsa
+fi
+echo ""
+
+echo ""
+echo "Launch Microsoft RDP via WindowsKey --> mstsc and enter your jumpbox servername:jumpbox-${serverPrefix}.eastus.cloudapp.azure.com" 
+echo "Demos will be found under \source\OSSonAzure"
+echo ""
