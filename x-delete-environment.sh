8332556a518930db597595d3e91ee10f8bffb77f
dansand71 <dansand71@hotmail.com>
2017-03-20 17:39:02 -0400

    220

--- x-delete-environment.sh
+++ x-delete-environment.sh
@@ -1,1 +1,5 @@
-~/bin/az group delete --name ossdemo-utility -y
+echo ""
+read -p "This will distroy the ossdemo-utility resource group.  This process is not reversable.  Continue? [y/n]:"  continuescript
+if [[ $continuescript == "y" ]];then
+    ~/bin/az group delete --name ossdemo-utility -y
+fi
