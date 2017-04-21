52d10dcc6bfdfb0e81cef74c3c1df31684fb65c8
Ray Kao <ray@peopleandcode.com>
2017-04-21 15:29:28 -0400

    Adding variables and defaults to script.  Allowing user to define azure region deployment.

--- bash_scripts/jumpbox_deploy.sh
+++ bash_scripts/jumpbox_deploy.sh
@@ -9,6 +9,10 @@ YELLOW="\033[38;5;11m"
 RED="\033[0;31m"
 DEBUG="no"
 
+AZ_RESOURCE_GROUP="ossdemo-utility"
+AZ_LOCATION="eastus"
+AZ_STORAGE_PREFIX=""
+
 clear
 
 echo -e "${BOLD}Set values for creation of resource groups and jumpbox server${RESET}"
@@ -18,22 +22,22 @@ echo ".Please enter your unique server prefix: (Jumpbox server will become:'jump
 echo "     (Note - values should be lowercase and less than 8 characters.)"
 read -p "$(echo -e -n "${INPUT}.Server Prefix:${RESET}")" serverPrefix
 # This requires a newer version of BASH not avialble in MAC OS - serverPrefix=${serverPrefix,,} 
-serverPrefix=$(echo "${serverPrefix}" | tr '[:upper:]' '[:lower:]')
+serverPrefix=$(echo "${AZ_SERVER_PREFIX}" | tr '[:upper:]' '[:lower:]')
 
 echo ".Please enter your new admin username:"
 echo "     (Note - values should be lowercase and less than 8 characters.)" 
-read -p "$(echo -e -n "${INPUT}.Admin Name:${RESET}")" serverAdminName
-# This requires a newer version of BASH not avialble in MAC OS - serverPrefix=${serverPrefix,,} 
-serverAdminName=$(echo "${serverAdminName}" | tr '[:upper:]' '[:lower:]')
+read -p "$(echo -e -n "${INPUT}.Admin Name:${RESET}")" JUMPBOX_SERVER_ADMIN_NAME
+# This requires a newer version of BASH not avialble in MAC OS - AZ_SERVER_PREFIX=${AZ_SERVER_PREFIX,,} 
+JUMPBOX_SERVER_ADMIN_NAME=$(echo "${JUMPBOX_SERVER_ADMIN_NAME}" | tr '[:upper:]' '[:lower:]')
 
 ### JUMPBOX SERVER PASSWORD
 while true
 do
   read -s -p "$(echo -e -n "${INPUT}.New Admin Password for Jumpbox:${RESET}")" jumpboxPassword
   echo ""
-  read -s -p "$(echo -e -n "${INPUT}.Re-enter to verify:${RESET}")" jumpboxPassword2
+  read -s -p "$(echo -e -n "${INPUT}.Re-enter to verify:${RESET}")" jumpboxPassword_confirm
   
-  if [ $jumpboxPassword = $jumpboxPassword2 ]
+  if [ $jumpboxPassword = $jumpboxPassword_confirm ]
   then
      break 2
   else
@@ -41,18 +45,14 @@ do
   fi
 done
 
-    #stty -echo
-    #    read -s jumpboxPassword
-    #stty echo
-
 # Check the validity of the name (no dashes, spaces, less than 8 char, no special chars etc..)"
 # Can we set a Enviro variable so if you want to rerun it is here and set by default?
 echo ".Please enter your unique storage prefix: (Storage Account will become: 'PREFIX-storage'')"
 echo "      (Note - values should be lowercase and less than 8 characters.)"
-read -p "$(echo -e -n "${INPUT}.Storage Prefix? (default: ${serverPrefix}demostorage):"${RESET})" storagePrefix
-[ -z "${storagePrefix}" ] && storagePrefix=${serverPrefix}
-# This requires a newer version of BASH not avialble in MAC OS - storagePrefix=${storagePrefix,,} 
-storagePrefix=$(echo "${storagePrefix}" | tr '[:upper:]' '[:lower:]')
+read -p "$(echo -e -n "${INPUT}.Storage Prefix? (default: ${AZ_SERVER_PREFIX}demostorage):"${RESET})" AZ_STORAGE_PREFIX
+[ -z "${AZ_STORAGE_PREFIX}" ] && AZ_STORAGE_PREFIX=${AZ_SERVER_PREFIX}
+# This requires a newer version of BASH not avialble in MAC OS - AZ_STORAGE_PREFIX=${AZ_STORAGE_PREFIX,,} 
+AZ_STORAGE_PREFIX=$(echo "${AZ_STORAGE_PREFIX}" | tr '[:upper:]' '[:lower:]')
 
 
 echo ""
@@ -62,21 +62,23 @@ if [[ $continuescript != "n" ]]; then
     #Make a copy of the template file
     cp ${SOURCEDIR}/environment/ossdemo-utility-template.json ${SOURCEDIR}/environment/ossdemo-utility.json -f
     #MODIFY line in JSON TEMPLATES
-    sudo sed -i -e "s@VALUEOF-UNIQUE-SERVER-PREFIX@${serverPrefix}@g" ${SOURCEDIR}/environment/ossdemo-utility.json
-    sudo sed -i -e "s@VALUEOF-UNIQUE-STORAGE-PREFIX@${storagePrefix}@g" ${SOURCEDIR}/environment/ossdemo-utility.json
+    sudo sed -i -e "s@VALUEOF-UNIQUE-SERVER-PREFIX@${AZ_SERVER_PREFIX}@g" ${SOURCEDIR}/environment/ossdemo-utility.json
+    sudo sed -i -e "s@VALUEOF-UNIQUE-STORAGE-PREFIX@${AZ_STORAGE_PREFIX}@g" ${SOURCEDIR}/environment/ossdemo-utility.json
 
     #BUILD RESOURCE GROUPS
     echo ".BUILDING RESOURCE GROUPS"
     echo "..Starting:"$(date)
     echo '..create utility resource group'
-    ~/bin/az group create --name ossdemo-utility --location eastus
+    read -p "$(echo -e -n "${INPUT}Resource group name: (default: ${AZ_RESOURCE_GROUP}):"${RESET})" AZ_RESOURCE_GROUP
+    read -p "$(echo -e -n "${INPUT}Deployment region: (default: ${AZ_LOCATION}):"${RESET})" AZ_LOCATION
+
+    ~/bin/az group create --name ossdemo-utility --location ${AZ_LOCATION}
 
     #APPLY TEMPLATE
-    echo ".APPLY JSON Template"
+    echo ".APPLY ARM Template"
     echo "..Starting:"$(date)
     echo '..Applying Network Security Group for utility Resource Group'
-    ~/bin/az group deployment create --resource-group ossdemo-utility --name InitialDeployment --template-file ${SOURCEDIR}/environment/ossdemo-utility.json
-
+    az group deployment create --resource-group ${AZ_RESOURCE_GROUP} --name InitialDeployment --template-file ${SOURCEDIR}/environment/ossdemo-utility.json
 fi
 
 echo ""
@@ -85,24 +87,24 @@ read -p "$(echo -e -n "${INPUT}Create jumpbox server? [Y/n]:"${RESET})" continue
 if [[ $continuescript != "n" ]]; then
     #Looking for jumpbox ssh key - if not found create one
     echo ".We are creating a new VM with SSH enabled.  Looking for an existing key or creating a new one."
-    if [ -f ~/.ssh/jumpbox_${serverPrefix}_id_rsa ]
+    if [ -f ~/.ssh/jumpbox_${AZ_SERVER_PREFIX}_id_rsa ]
     then
-        echo "..Existing private key found.  Using this key ~/.ssh/jumpbox_${serverPrefix}_id_rsa for jumpbox creation"
+        echo "..Existing private key found.  Using this key ~/.ssh/jumpbox_${AZ_SERVER_PREFIX}_id_rsa for jumpbox creation"
     else
-        echo "..Creating new key for ssh in ~/.ssh/jumpbox_${serverPrefix}_id_rsa"
+        echo "..Creating new key for ssh in ~/.ssh/jumpbox_${AZ_SERVER_PREFIX}_id_rsa"
         #Create key
-        ssh-keygen -f ~/.ssh/jumpbox_${serverPrefix}_id_rsa -N "" -q
+        ssh-keygen -f ~/.ssh/jumpbox_${AZ_SERVER_PREFIX}_id_rsa -N "" -q
         #Add this key to the ssh config file 
     fi
-    if grep -Fxq "Host jumpbox-${serverPrefix}.eastus.cloudapp.azure.com" ~/.ssh/config
+    if grep -Fxq "Host jumpbox-${AZ_SERVER_PREFIX}.eastus.cloudapp.azure.com" ~/.ssh/config
     then
         # Replace the server with the right private key
         # BUG BUG - we need to actually replace the next three lines with new values
-        # sed -i "s@*Host jumpbox-${serverPrefix}.eastus.cloudapp.azure.com*@Host=jumpbox-${serverPrefix}.eastus.cloudapp.azure.com IdentityFile=~/.ssh/jumpbox_${serverPrefix}_id_rsa User=${serverAdminName}@g" ~/.ssh/config
+        # sed -i "s@*Host jumpbox-${AZ_SERVER_PREFIX}.eastus.cloudapp.azure.com*@Host=jumpbox-${AZ_SERVER_PREFIX}.eastus.cloudapp.azure.com IdentityFile=~/.ssh/jumpbox_${AZ_SERVER_PREFIX}_id_rsa User=${JUMPBOX_SERVER_ADMIN_NAME}@g" ~/.ssh/config
         echo "..We found an entry in ~/.ssh/config for this server - do not recreate."
     else
         # Add this to the config file
-        echo -e "Host=jumpbox-${serverPrefix}.eastus.cloudapp.azure.com\nIdentityFile=~/.ssh/jumpbox_${serverPrefix}_id_rsa\nUser=${serverAdminName}" >> ~/.ssh/config
+        echo -e "Host=jumpbox-${serverPrefix}.eastus.cloudapp.azure.com\nIdentityFile=~/.ssh/jumpbox_${serverPrefix}_id_rsa\nUser=${JUMPBOX_SERVER_ADMIN_NAME}" >> ~/.ssh/config
     fi
 
     sudo chmod 600 ~/.ssh/config
@@ -123,7 +125,7 @@ if [[ $continuescript != "n" ]]; then
     --nsg NSG-ossdemo-utility  \
     --storage-sku Premium_LRS --size Standard_DS2_v2 \
     --vnet-name ossdemos-vnet --subnet ossdemo-utility-subnet \
-    --admin-username ${serverAdminName} \
+    --admin-username ${JUMPBOX_SERVER_ADMIN_NAME} \
     --ssh-key-value ~/.ssh/jumpbox_${serverPrefix}_id_rsa.pub "
 
     echo "..Calling creation command: ~/bin/az vm create ${azcreatecommand}"
@@ -141,7 +143,7 @@ echo ".Starting:"$(date)
 cp ${SOURCEDIR}/ansible/jumpbox-server-configuration-template.yml ${SOURCEDIR}/ansible/jumpbox-server-configuration.yml -f
 cp ${SOURCEDIR}/ansible/hosts-template ${SOURCEDIR}/ansible/hosts -f
 sudo sed -i -e "s@JUMPBOXSERVER-REPLACE.eastus.cloudapp.azure.com@jumpbox-${serverPrefix}.eastus.cloudapp.azure.com@g" ${SOURCEDIR}/ansible/hosts
-sudo sed -i -e "s@VALUEOF_DEMO_ADMIN_USER@${serverAdminName}@g" ${SOURCEDIR}/ansible/jumpbox-server-configuration.yml
+sudo sed -i -e "s@VALUEOF_DEMO_ADMIN_USER@${JUMPBOX_SERVER_ADMIN_NAME}@g" ${SOURCEDIR}/ansible/jumpbox-server-configuration.yml
 
 echo ""
 echo "---------------------------------------------"
@@ -152,19 +154,19 @@ sudo sed -i -e "s@JUMPBOX_SERVER_NAME=@JUMPBOX_SERVER_NAME=jumpbox-${serverPrefi
 sudo sed -i -e "s@DEMO_SERVER_PREFIX=@DEMO_SERVER_PREFIX=${serverPrefix}@g" ${SOURCEDIR}/vm-assets/DemoEnvironmentValues
 sudo sed -i -e "s@DEMO_STORAGE_ACCOUNT=@DEMO_STORAGE_ACCOUNT=${storagePrefix}storage@g" ${SOURCEDIR}/vm-assets/DemoEnvironmentValues
 sudo sed -i -e "s@DEMO_STORAGE_PREFIX=@DEMO_STORAGE_PREFIX=${storagePrefix}@g" ${SOURCEDIR}/vm-assets/DemoEnvironmentValues
-sudo sed -i -e "s@DEMO_ADMIN_USER=@DEMO_ADMIN_USER=${serverAdminName}@g" ${SOURCEDIR}/vm-assets/DemoEnvironmentValues
+sudo sed -i -e "s@DEMO_ADMIN_USER=@DEMO_ADMIN_USER=${JUMPBOX_SERVER_ADMIN_NAME}@g" ${SOURCEDIR}/vm-assets/DemoEnvironmentValues
 
 #Set the remote jumpbox passwords
-echo "Resetting ${serverAdminName} and root passwords based on script values."
+echo "Resetting ${JUMPBOX_SERVER_ADMIN_NAME} and root passwords based on script values."
 echo "Starting:"$(date)
-ssh -t -o BatchMode=yes -o StrictHostKeyChecking=no ${serverAdminName}@jumpbox-${serverPrefix}.eastus.cloudapp.azure.com -i ~/.ssh/jumpbox_${serverPrefix}_id_rsa "echo '${serverAdminName}:${jumpboxPassword}' | sudo chpasswd"
-ssh -t -o BatchMode=yes -o StrictHostKeyChecking=no ${serverAdminName}@jumpbox-${serverPrefix}.eastus.cloudapp.azure.com -i ~/.ssh/jumpbox_${serverPrefix}_id_rsa "echo 'root:${jumpboxPassword}' | sudo chpasswd"
+ssh -t -o BatchMode=yes -o StrictHostKeyChecking=no ${JUMPBOX_SERVER_ADMIN_NAME}@jumpbox-${serverPrefix}.eastus.cloudapp.azure.com -i ~/.ssh/jumpbox_${serverPrefix}_id_rsa "echo '${JUMPBOX_SERVER_ADMIN_NAME}:${jumpboxPassword}' | sudo chpasswd"
+ssh -t -o BatchMode=yes -o StrictHostKeyChecking=no ${JUMPBOX_SERVER_ADMIN_NAME}@jumpbox-${serverPrefix}.eastus.cloudapp.azure.com -i ~/.ssh/jumpbox_${serverPrefix}_id_rsa "echo 'root:${jumpboxPassword}' | sudo chpasswd"
 
 #Copy the SSH private & public keys up to the jumpbox server
-echo "Copying up the SSH Keys for demo purposes to the jumpbox ~/.ssh directories for ${serverAdminName} user."
+echo "Copying up the SSH Keys for demo purposes to the jumpbox ~/.ssh directories for ${JUMPBOX_SERVER_ADMIN_NAME} user."
 echo "Starting:"$(date)
-scp ~/.ssh/jumpbox_${serverPrefix}_id_rsa ${serverAdminName}@jumpbox-${serverPrefix}.eastus.cloudapp.azure.com:~/.ssh/id_rsa
-scp ~/.ssh/jumpbox_${serverPrefix}_id_rsa.pub ${serverAdminName}@jumpbox-${serverPrefix}.eastus.cloudapp.azure.com:~/.ssh/id_rsa.pub
+scp ~/.ssh/jumpbox_${serverPrefix}_id_rsa ${JUMPBOX_SERVER_ADMIN_NAME}@jumpbox-${serverPrefix}.eastus.cloudapp.azure.com:~/.ssh/id_rsa
+scp ~/.ssh/jumpbox_${serverPrefix}_id_rsa.pub ${JUMPBOX_SERVER_ADMIN_NAME}@jumpbox-${serverPrefix}.eastus.cloudapp.azure.com:~/.ssh/id_rsa.pub
 ssh -t -o BatchMode=yes -o StrictHostKeyChecking=no ${serverAdminName}@jumpbox-${serverPrefix}.eastus.cloudapp.azure.com -i ~/.ssh/jumpbox_${serverPrefix}_id_rsa 'sudo chmod 600 ~/.ssh/id_rsa'
 
 #mkdir for source on jumpbox server
