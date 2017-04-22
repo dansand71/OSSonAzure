9e3db388cd9427235ca00f6dbfb7ec6831834472
Ray Kao <ray@peopleandcode.com>
2017-04-22 01:15:03 -0400

    New Jumbox deploy script includes additional defaults and some console output.

--- bash_scripts/02-deploy-jumpbox.new.sh
+++ bash_scripts/02-deploy-jumpbox.new.sh
@@ -12,11 +12,21 @@ DEBUG="no"
 JUMPBOX_SERVER_PREFIX="ossdemo"
 JUMPBOX_ADMIN_NAME="kenobi"
 JUMPBOX_ADMIN_PASSWORD="H3ll0Ben!"
+JUMPBOX_OUTPUT_FILE="jumpbox.deployment.output.json"
 
 # Azure deployment variables defaults
 AZ_RESOURCE_GROUP="ossdemo"
 AZ_LOCATION="eastus"
+ARM_TEMPLATE_FOLDER="arm_templates/"
+ARM_TEMPLATE_FILE=$ARM_TEMPLATE_FOLDER"jumpbox.arm.json"
+ARM_PARAMETERS_FILE=$ARM_TEMPLATE_FOLDER"jumpbox.parameters.json"
 
 # create Azure Resource Group
+printf "Creating Resouce Group...\n"
 az group create --name ${AZ_RESOURCE_GROUP} --locaiton ${AZ_LOCATION}
-az group deployment create --name test-ossdemo --resource-group ${AZ_RESOURCE_GROUP} --template-file arm_templates/jumpbox.arm.json >> jumpbox.deployment.output.json
+printf "Done.\n\n"
+
+printf "Deploying ARM Template...\n"
+az group deployment create --name test-ossdemo --resource-group ${AZ_RESOURCE_GROUP} --template-file ${ARM_TEMPLATE_FILE} > $JUMPBOX_OUTPUT_FILE
+printf "Done."
+printf "Deployment output can be found in '${JUMPBOX_OUTPUT_FILE}'\n\n"
