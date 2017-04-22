e5fc191f51d59f062fa0cb61b85da2835d58c6fa
Ray Kao <ray@peopleandcode.com>
2017-04-22 01:57:05 -0400

    Fixed servername concatenation issue in arm template.
    Updating new deploy script

--- bash_scripts/02-deploy-jumpbox.new.sh
+++ bash_scripts/02-deploy-jumpbox.new.sh
@@ -13,6 +13,7 @@ JUMPBOX_SERVER_PREFIX="ossdemo"
 JUMPBOX_ADMIN_NAME="kenobi"
 JUMPBOX_ADMIN_PASSWORD="H3ll0Ben!"
 JUMPBOX_OUTPUT_FILE="jumpbox.deployment.output.json"
+JUMPBOX_FQDN=""
 
 # Azure deployment variables defaults
 AZ_RESOURCE_GROUP="ossdemo"
@@ -22,24 +23,29 @@ AZ_LOCATION="eastus"
 ARM_TEMPLATE_FOLDER="arm_templates"
 ARM_TEMPLATE_FILENAME="jumpbox.arm.json"
 ARM_PARAMETERS_FILENAME="jumpbox.parameters.json"
-ARM_TEMPLATE_FILE_LOCATION=$ARM_TEMPLATE_FOLDER"/"$ARM_TEMPLATE_FILENAME
+ARM_TEMPLATE_FILE=$ARM_TEMPLATE_FOLDER"/"$ARM_TEMPLATE_FILENAME
 ARM_PARAMETERS_FILE=$ARM_TEMPLATE_FOLDER"/"$ARM_PARAMETERS_FILENAME
 
 # create Azure Resource Group
-printf "Creating Resouce Group...\n"
-az group create --name ${AZ_RESOURCE_GROUP} --locaiton ${AZ_LOCATION}
-printf "Done.\n\n"
+echo -e "Creating Resouce Group...\n"
+az group create --name ${AZ_RESOURCE_GROUP} --location ${AZ_LOCATION}
+echo -e "Done.\n\n"
 
 # generate Azure Template Parameters
-cp ${ARM_TEMPLATE_FOLDER}jumbox.parameters.json.example ${ARM_PARAMETERS_FILE}
-printf "Generated Parameters File for Jumpbox ARM Template\n\n"
-printf "Updating Parameters...\n"
-# todo: replace default values with user inputed values
+cp ${ARM_TEMPLATE_FOLDER}jumpbox.parameters.json.example ${ARM_PARAMETERS_FILE}
+echo -e "Generated Parameters File for Jumpbox ARM Template\n\n"
+echo -e "Updating Parameters...\n"
+
+
 
 # deploy Azure Template
-printf "Deploying ARM Template...\n"
+echo -e "Deploying ARM Template...\n"
 az group deployment create --name test-ossdemo --resource-group ${AZ_RESOURCE_GROUP} --template-file ${ARM_TEMPLATE_FILE} > $JUMPBOX_OUTPUT_FILE
-printf "Done."
-printf "Deployment output can be found in '${JUMPBOX_OUTPUT_FILE}'\n\n"
+echo -e "Jumpbox deployed."
+echo -e "Deployment output can be found in '${JUMPBOX_OUTPUT_FILE}'\n\n"
 
-cat jumpbox.deployment.output.json | jq -r '.properties.outputs.dnsName.value'
+# output new server FQDN
+JUMPBOX_FQDN=$(cat jumpbox.deployment.output.json | jq -r '.properties.outputs.dnsName.value')
+echo -e "Server can be reached at: $JUMPBOX_FQDN\n"
+echo -e "ssh $JUMPBOX_ADMIN_NAME@$JUMPBOX_FQDN\n\n"
+
