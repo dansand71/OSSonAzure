f797bb6d8b4fc8b6d27ce03c71a3e446566b8fc1
Ray Kao <ray@peopleandcode.com>
2017-04-26 15:09:23 -0400

    Updated ability to create/use/change resource group on script run

--- bash_scripts/02-deploy-jumpbox.new.sh
+++ bash_scripts/02-deploy-jumpbox.new.sh
@@ -12,9 +12,10 @@ then
 else
     az account list > accounts.json
 fi
+## END ##
 
 
-## Change Subscription
+# Change Subscription?
 echo -e "Current subscription is: " $(jq -r ".[] | select(.isDefault) | .name" accounts.json) ":" $(jq ".[] | select(.isDefault) | .id" accounts.json) "\n"
 read -p "Change to a different subscription? (y/N)" promptChangeSubscription
 promptChangeSubscription=$(echo "${promptChangeSubscription}" | tr '[:upper:]' '[:lower:]')
@@ -34,23 +35,59 @@ then
 
     echo -e "Subscription changed to: $(az account list | jq -r ".[] | select(.isDefault) | .name + \" (\" + .id + \")\" " )"
 fi
-
-# create Azure Resource Group
-echo -e "Creating Resouce Group...\n"
-az group create \
-    --name ${AZ_RESOURCE_GROUP} \
-    --location ${AZ_LOCATION}
-
-echo -e "Done.\n\n"
+## END ##
 
 
+# Use default Resource Group?
+az_rg_exists=$(az group show --name ${AZ_RESOURCE_GROUP_NAME} | jq '.name')
+
+if [ -z "$az_rg_exists" ];
+then 
+    read -p "Resource Group '$AZ_RESOURCE_GROUP_NAME' does not exist.  Create and use group '$AZ_RESOURCE_GROUP_NAME'? (Y/n):" useOrCreateResourceGroup    
+else 
+    read -p "Use Resource Group ${AZ_RESOURCE_GROUP_NAME}? (Y/n): " useOrCreateResourceGroup
+fi
+
+useOrCreateResourceGroup=$(echo "${useOrCreateResourceGroup}" | tr '[:upper:]' '[:lower:]')
+
+if [[ $useOrCreateResourceGroup = 'y' &&  -z "$az_rg_exists" ]];
+then
+    read -p "Please enter the Azure Region to deploy Resource Group to (default '$AZ_LOCATION)'):" AZ_LOCATION
+    # create Azure Resource Group
+
+    echo -e "Creating Resouce Group...\n"
+    az group create \
+        --name ${AZ_RESOURCE_GROUP_NAME} \
+        --location ${AZ_LOCATION}
+        
+    echo -e "Resource Group ${AZ_RESOURCE_GROUP_NAME} created in ${AZ_LOCATION}.\n\n"
+fi
+
+if [[ $useOrCreateResourceGroup = 'n' ]];
+then
+    echo -e "Select another Resource Group to use:"
+
+    az group list > resourceGroupList.json
+    resourceGroupLength=$(jq 'length' resourceGroupList.json)
+    
+    for ((resourceGroupIndex=0; resourceGroupIndex<resourceGroupLength; resourceGroupIndex++))
+    do
+        echo "$(expr $resourceGroupIndex + 1)."  $(jq -r ".[${resourceGroupIndex}] | .name" resourceGroupList.json)
+    done
+
+    read -p "Choose Resource Group (1 - $resourceGroupLength): " resourceGroupChoice
+    resourceGroupChoice=$(expr $resourceGroupChoice - 1)
+    AZ_RESOURCE_GROUP_NAME=$(jq -r ".[$resourceGroupChoice] | .name" resourceGroupList.json)
+fi
+
+echo -e "Now using Resource Group: $AZ_RESOURCE_GROUP_NAME"
+## END ##
 
 # generate Azure Template Parameters
 cp ${ARM_TEMPLATE_FOLDER}/jumpbox.parameters.json.example ${ARM_PARAMETERS_FILE}
 echo -e "Generated Parameters File for Jumpbox ARM Template\n\n"
 echo -e "Updating Parameters...\n"
-
-
+## END ##
 
 # deploy Azure Template
 echo -e "Deploying ARM Template...\n"
@@ -58,11 +95,13 @@ az group deployment create \
     --name test-ossdemo \
     --resource-group ${AZ_RESOURCE_GROUP} \
     --template-file ${ARM_TEMPLATE_FILE}
+    --parameters-file ${ARM_PARAMETERS_FILE}
 
 if [ $? = 0 ];
 then
 	echo -e "Jumpbox deployed."
 fi
+## END ##
 
 # echo -e "Deployment output can be found in '${JUMPBOX_OUTPUT_FILE}'\n\n"
 
